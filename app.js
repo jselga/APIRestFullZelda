7de6b80b4686119a9c6c4316707dd0670960a8fe
jselga <jordi.selga@iescarelesvallbona.cat>
2025-03-14 21:23:37 +0100

    Millora PUT amb gestio d'errors

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const dbconnect = require('./config');
 const MaterialModel = require('./materialModel');
 const app = express();
 const router = express.Router();
-
-router.get("/materials", async(req, res) => {
+const msgError404 = "Material no encontrado";
+const msgErrorSrv = "Error del servidor";
+router.get("/materials", async (req, res) => {
     const resposta = await MaterialModel.find({});
     res.send(resposta);
 })
 router.get("/materials/:id_num", async (req, res) => {
     const idn = req.params.id_num;
-    const resposta = await MaterialModel.findOne({id_num:idn});
+    const resposta = await MaterialModel.findOne({ id_num: idn });
     res.send(resposta);
 })
 
@@ -22,16 +23,27 @@ router.post("/materials", async (req, res) => {
 
 
 
-router.put("/materials/:id_num",async (req,res)=>{
-    const body = req.body;
-    const idn = req.params.id_num;
-    const resposta = await MaterialModel.findOneAndUpdate({id_num:idn}, body)
-    res.send(resposta);
+router.put("/materials/:id_num", async (req, res) => {
+    try {
+        const body = req.body;
+        const idn = req.params.id_num;
+        const resposta = await MaterialModel.findOneAndUpdate(
+            { id_num: idn },
+            { $set: body },
+            { new: true, runValidators: true }
+        )
+        if (!resposta) {
+            return res.status(404).json({ message: msgError404 });
+        }
+        res.send(resposta);
+    } catch (error) {
+        res.status(500).json({ message: msgErrorSrv });
+    }
 })
 
 router.delete("/materials/:id_num", async (req, res) => {
     const idn = req.params.id_num;
-    const resposta = await MaterialModel.deleteOne({id_num:idn});
+    const resposta = await MaterialModel.deleteOne({ id_num: idn });
     res.send(resposta);
 })
 app.use(express.json())
