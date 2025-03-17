2e00852dcc1a7efa9a54e780d6c783fd2a0fd9ea
jselga <jordi.selga@iescarelesvallbona.cat>
2025-03-17 12:56:49 +0100

    Refactor del codi amb gestio d'errors

--- app.js
+++ app.js
@@ -5,22 +5,57 @@ const app = express();
 const router = express.Router();
 const msgError404 = "Material no encontrado";
 const msgErrorSrv = "Error del servidor";
+const msgOkDelete ="Material eliminado correctamente";
 const cors = require('cors')
 app.use(cors());
+app.use(express.json());
 router.get("/materials", async (req, res) => {
-    const resposta = await MaterialModel.find({});
-    res.send(resposta);
+    try {
+        const resposta = await MaterialModel.find({});
+        res.send(resposta);
+    } catch (error) {
+        res.status(500).json({ message: msgErrorSrv });
+    }
 })
 router.get("/materials/:id_num", async (req, res) => {
-    const idn = req.params.id_num;
-    const resposta = await MaterialModel.findOne({ id_num: idn });
-    res.send(resposta);
+    try {
+        const idn = req.params.id_num;
+        const resposta = await MaterialModel.findOne({ id_num: idn });
+        if (!resposta) {
+            return res.status(404).json({ message: msgError404 });
+        }
+        res.send(resposta); 
+    } catch (error) {
+        res.status(500).json({ message: msgErrorSrv });
+    }
+    
+})
+router.get("/materials/name/:name",async (req,res)=>{
+    try {
+        const nom = req.params.name.toLowerCase(); 
+
+        const resposta = await MaterialModel.findOne({ name: new RegExp(`^${nom}$`, "i") }); // Cerca insensible a majúscules
+
+        if (!resposta) {
+            return res.status(404).json({ message: msgError404 });
+        }
+
+        res.json(resposta);
+    } catch (error) {
+        res.status(500).json({ message: msgErrorSrv, error });
+    }
 })
 
 router.post("/materials", async (req, res) => {
-    const body = req.body;
-    const resposta = await MaterialModel.create(body)
-    res.send(resposta);
+    try {
+        const body = req.body;
+        const resposta = await MaterialModel.create(body);
+        res.status(201).json(resposta);
+    } catch (error) {
+        res.status(500).json({ message: msgErrorSrv, error });
+    }
+ 
+
 })
 
 
@@ -44,14 +79,26 @@ router.put("/materials/:id_num", async (req, res) => {
 })
 
 router.delete("/materials/:id_num", async (req, res) => {
-    const idn = req.params.id_num;
-    const resposta = await MaterialModel.deleteOne({ id_num: idn });
-    res.send(resposta);
-})
-app.use(express.json())
-app.use(router);
-app.listen(3001, () => {
-    console.log("El servidor està en el port 3001");
+    try {
+        const idn = req.params.id_num;
+        const resposta = await MaterialModel.findOneAndDelete({ id_num: idn });
+
+        if (!resposta) {
+            return res.status(404).json({ message: msgError404 });
+        }
+
+        res.json({ message: msgOkDelete });
+    } catch (error) {
+        res.status(500).json({ message: msgErrorSrv, error });
+    }
+});
+
+
+
+app.use("/",router);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`El servidor està en el port ${PORT}`);
 
 })
 dbconnect();
